f119f860d530dacc3d4b2fea619caa5eb4cf5f80
yarariahi <63755651+yarariahi@users.noreply.github.com>
2021-07-05 11:22:10 -0700

    Update duelGame.cpp

--- duelGame.cpp
+++ duelGame.cpp
@@ -5,21 +5,18 @@ using namespace std;
 #include <time.h>
 #include <unistd.h>
 int flag = 0;
-
+bool flag1 = false;
 
     class Human{
         public: 
-            int attack;
+            int attack= 10;
             int health;
             bool dead = false;
             bool armor = true;
             
             void Attack(){
-                
-                srand(time(0));
                 attack = rand();
                 attack = attack % 20;
-                //cout<<"human attack "<<attack<<endl;
             }
             
              
@@ -36,56 +33,82 @@ int flag = 0;
              
            
             void Attack(){
+                srand(time(0));
                 attack = rand();
                 attack = attack % 20;
-                
-                //cout<<"monster attack "<<attack<<endl;
-                
             }
         
     };
 
+void Check(Human warrior, Monster badguy){
+        
     
-
-void Duel(Human &warrior, Monster &badguy){
-
-    badguy.health = badguy.health - warrior.attack;
-    cout<<"The monster's health is "<<badguy.health<<endl;
-    
-    if(badguy.attack >= 15){
-        if(flag == 0){
-            cout<<"The monster has broken your armor"<<endl;
-            warrior.armor = false;
-            flag = 1;
-        }   
-    }
-    
-   
-    if(warrior.armor == false){
-        warrior.health = warrior.health - (badguy.attack * 1.5);
-        cout<<"Your health is "<<warrior.health<<endl;
-    }
-    else if(warrior.armor == true){
-        warrior.health = warrior.health - badguy.attack;
-        cout<<"Your health is "<<warrior.health<<endl; 
-    }
-    
-    
-    
-    if(warrior.health <= 0){
-        warrior.dead = true;
-    }
-    if(badguy.health <= 0){
+    if(warrior.health <= 0 && badguy.health <= 0){
+        cout<<"You both died at the same time"<<endl;
         badguy.dead = true;
+        warrior.dead = true;
+        flag1 = true;
+        return;
     }
-    // int counter = 1;
-    // while(counter < 200000000){
-    //     counter++;
-    //}
-    usleep(1000000);
+    else if(warrior.health <= 0){
+        cout<<"You died..."<<endl;
+        warrior.dead = true;
+        flag1 = true;
+        return;
+    }
+    else if(badguy.health <= 0){
+        cout<<"You killed the monster!"<<endl;
+        badguy.dead = true;
+        flag1 = true;
+        return;
+    }
+}    
+
+void Duel(Human warrior, Monster badguy){
+
+    while(badguy.dead == false || warrior.dead == false){
+        
+        cout<<flag1<<endl;
+        
+        Check(warrior, badguy);
+        if(flag1 == true){
+            return;
+        }
+        
+        badguy.health = badguy.health - warrior.attack;
+        cout<<"The monster's health is "<<badguy.health<<endl;
+        
+        usleep(500000);
+        
+
+        
+        if(badguy.attack >= 15){
+            if(flag == 0){
+                cout<<"The monster has broken your armor"<<endl;
+                warrior.armor = false;
+                flag = 1;
+            }   
+        }
+        
+       
+        if(warrior.armor == false){
+            warrior.health = warrior.health - (badguy.attack * 1.5);
+            cout<<"Your health is "<<warrior.health<<endl;
+        }
+        else if(warrior.armor == true){
+            warrior.health = warrior.health - badguy.attack;
+            cout<<"Your health is "<<warrior.health<<endl; 
+        }
+        
+        Check(warrior, badguy);
+        if(flag1 == true){
+            return;
+        }
+       
+        usleep(500000);
+
+    }
 }
-
-
 
 int main()
 {
@@ -93,33 +116,14 @@ int main()
         Human warrior;
         Monster badguy;
         
-        
         warrior.health = 100;
         badguy.health = 100;
-        
-        
-   while(warrior.dead == false){
        
         warrior.Attack();
         badguy.Attack();
         
         Duel(warrior, badguy);
-        
-      
-        
-        if(warrior.dead == true){
-            cout<<"You died"<<endl;
-            break;
-        }
-        if(badguy.dead == true){
-            cout<<"You killed the monster, congrats"<<endl;
-            break;
-        }   
-            
-    }
 
    return 0;
 }
-
-
 
